89be2e4837bb401b0a49133818f53591ae277638
bilal ahmad <bilalahmad.pu@gmail.com>
2011-12-14 01:59:07 +0500

    manage pages removed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '260685093987581', 'e151eec9f06f59b97fad93c072258497', {client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}, :scope => 'publish_stream, offline_access, email, manage_pages, create_event, rsvp_event, xmpp_login'}
+  provider :facebook, '317467998278508', 'edb0f470addae80c6ca67b31d3b5b58f', {client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}, :scope => 'publish_stream, offline_access, email, create_event, rsvp_event, xmpp_login'}
 end
