8e4250bd294e3ba6eeac790c58e4e56df7001b19
bilal ahmad <bilalahmad.pu@gmail.com>
2011-12-15 21:47:18 +0500

    app settings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,11 @@
+if Rails.env.production?
+  APP_ID = "260685093987581"
+  APP_SECRET = "e151eec9f06f59b97fad93c072258497"
+elsif Rails.env.development?
+  APP_ID = "317467998278508"
+  APP_SECRET = "edb0f470addae80c6ca67b31d3b5b58f"
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '317467998278508', 'edb0f470addae80c6ca67b31d3b5b58f', {client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}, :scope => 'publish_stream, offline_access, email, create_event, rsvp_event, xmpp_login'}
+  provider :facebook, APP_ID, APP_SECRET, {client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}, :scope => 'publish_stream, offline_access, email, create_event, rsvp_event, xmpp_login'}
 end
