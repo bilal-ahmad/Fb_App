111c0bfd064be572f098376b135dea9877372c13
bilal ahmad <bilalahmad.pu@gmail.com>
2011-12-16 01:01:02 +0500

    facebook permission

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
-  #before_filter :register_user
+  before_filter :register_user
   def index
     @auth_url = get_oauth_url if !current_user
 
+  end
+
+  def register_user
+    redirect_to get_app_url if current_user
   end
 
 end
