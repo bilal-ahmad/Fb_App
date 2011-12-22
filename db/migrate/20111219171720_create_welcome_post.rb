95be171998de859fec583b2678628c1a38192bb4
bilal ahmad <bilalahmad.pu@gmail.com>
2011-12-22 23:22:32 +0500

    same

--- db/migrate/20111219171720_create_welcome_post.rb
+++ db/migrate/20111219171720_create_welcome_post.rb
@@ -1,6 +1,6 @@
 class CreateWelcomePost < ActiveRecord::Migration
   def up
-    execute "INSERT INTO social_posts  VALUES (5, 'welcome', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);"
+    execute "INSERT INTO social_posts  VALUES (6, 'welcome', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);"
   end
 
   def down
