c1c289943abbae2193baa782c05bba47f32d66fb
bilal ahmad <bilalahmad.pu@gmail.com>
2012-01-03 22:36:02 +0500

    reset facebook key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   APP_ID = "258921684173624"
-  APP_SECRET = "318b084ade89f3abe35ae1e3b435f754"
+  APP_SECRET = "4f8938d6b046f1314cb5326a2d122ff9"
 elsif Rails.env.development?
   APP_ID = "317467998278508"
   APP_SECRET = "edb0f470addae80c6ca67b31d3b5b58f"
