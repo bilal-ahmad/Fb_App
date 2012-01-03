fb3dbe1400c7b5aa91ba7f05d79054bedfeb316d
bilal ahmad <bilalahmad.pu@gmail.com>
2012-01-03 23:31:22 +0500

    new keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
-  APP_ID = "301989419836537"
-  APP_SECRET = "b5f085da67f5043cce611ba0220a09af"
+  APP_ID = "162625303842724"
+  APP_SECRET = "332f48a6aa67cd59cfa05b1a3a035632"
 elsif Rails.env.development?
   APP_ID = "317467998278508"
   APP_SECRET = "edb0f470addae80c6ca67b31d3b5b58f"
