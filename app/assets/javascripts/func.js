df0e9e062802c7bdcd25b38013bb1fb4c81175d5
bilal ahmad <bilalahmad.pu@gmail.com>
2012-03-14 00:09:19 +0500

    same

--- app/assets/javascripts/func.js
+++ app/assets/javascripts/func.js
@@ -1,32 +1,32 @@
-jQuery(document).ready(function() {
-	jQuery('.slider').jcarousel( {
-		initCallback: slider_initCallback,
-		itemFirstInCallback: slider_firstInCallback,
-		scroll: 1,
-		auto: 5,
-		wrap: 'both',
-		// This tells jCarousel NOT to autobuild prev/next buttons
-		buttonNextHTML: null,
-		buttonPrevHTML: null
-	});
-});
-
-function slider_initCallback (carousel) {
-	
-	jQuery('.slider-nav a').bind('click', function() {
-		carousel.scroll($.jcarousel.intval($(this).text()));
-		return false;
-	});
-}
-
-function slider_firstInCallback(carousel, item, idx, state) {
-	jQuery('.slider-nav a').removeClass('active');
-	jQuery('.slider-nav a').eq(idx-1).addClass('active');
-}
-
-
-
-
+//jQuery(document).ready(function() {
+//	jQuery('.slider').jcarousel( {
+//		initCallback: slider_initCallback,
+//		itemFirstInCallback: slider_firstInCallback,
+//		scroll: 1,
+//		auto: 5,
+//		wrap: 'both',
+//		// This tells jCarousel NOT to autobuild prev/next buttons
+//		buttonNextHTML: null,
+//		buttonPrevHTML: null
+//	});
+//});
+//
+//function slider_initCallback (carousel) {
+//
+//	jQuery('.slider-nav a').bind('click', function() {
+//		carousel.scroll($.jcarousel.intval($(this).text()));
+//		return false;
+//	});
+//}
+//
+//function slider_firstInCallback(carousel, item, idx, state) {
+//	jQuery('.slider-nav a').removeClass('active');
+//	jQuery('.slider-nav a').eq(idx-1).addClass('active');
+//}
+//
+//
+//
+//
 $('#fb_div').click(function() {
     alert("hi");
     $('#facebook_accounts').toggle();
@@ -35,7 +35,7 @@ $('#fb_div').click(function() {
 function social_post(id){
     //alert(id);
     url = "http://summacbook.info/post_to_wall";
-    $.post(url, { post_type: "default" , _method: 'post', 'user_id': id },
+    jQuery.post(url, { post_type: "default" , _method: 'post', 'user_id': id },
         function(data) {
             alert("Data Loaded: " + data);
         });
